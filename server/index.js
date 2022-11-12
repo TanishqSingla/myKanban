1b42c3170e656bc494ca2883aa102aa1fdfb2cb1
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-13 02:51:18 +0530

    Enabling cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const dbURL = "mongodb://127.0.0.1:27017/myKanban";
 
@@ -9,10 +10,14 @@ const userRouter = require("./routes/userRouter");
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser);
+app.use(cookieParser());
+app.use(cors());
 app.use("/user", userRouter);
 
-mongoose.connect(dbURL).then(() => console.log("db connected")).catch(e => console.log(e));
+mongoose
+	.connect(dbURL)
+	.then(() => console.log("db connected"))
+	.catch((e) => console.log(e));
 
 app.listen(4000, () =>
 	console.log("server running on port http://localhost:4000")
