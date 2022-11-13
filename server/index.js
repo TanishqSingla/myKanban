ee394cf86213f8c16dd5cda217e79ff5cac5068c
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-14 02:44:48 +0530

    Shift api routes under /api

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:3000"] }));
-app.use("/user", userRouter);
+app.use("/api/user", userRouter);
 
 mongoose
 	.connect(dbURL)
