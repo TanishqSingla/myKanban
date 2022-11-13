53fcf25d482d2d1505a45254723f2df9695f97e8
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-14 05:27:15 +0530

    Add authenticated route
    Add route to return the user authentication status

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,8 +1,8 @@
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
-function createJWT(id) {
-	return jwt.sign({ id }, "my secret");
+function createJWT(_id) {
+	return jwt.sign({ _id }, "my secret");
 }
 
 module.exports.signup = async (req, res) => {
@@ -12,8 +12,8 @@ module.exports.signup = async (req, res) => {
 
 		const token = createJWT(user._id);
 
-    res.cookie('jwt', token);
-		res.json({user, token});
+		res.cookie("jwt", token);
+		res.json({ user, token });
 	} catch (err) {
 		console.log(err);
 		res.status(503).json(err);
@@ -24,4 +24,20 @@ module.exports.login = (req, res) => {
 	const { email, password } = req.body;
 	console.log(email, password);
 	res.status("sign in").json();
+};
+
+module.exports.authenticated = (req, res) => {
+	const token = req.cookies?.jwt;
+	if (!token) {
+		res.json({ isAuthenticated: false });
+		return;
+	}
+	jwt.verify(token, "my secret", (err, user) => {
+		if (err) {
+			res.json({ isAuthenticated: false });
+			return;
+		} else {
+			res.json({ isAuthenticated: true, _id: user._id});
+		}
+	});
 };
