199cf6ccd47d1b44571f9cf17449da3f7593d686
TanishqSingla <tanishqsingla00@gmail.com>
2022-11-16 01:56:32 +0530

    Fix: creating board not async

--- server/controllers/actionController.js
+++ server/controllers/actionController.js
@@ -18,8 +18,8 @@ module.exports.createBoard = async (req, res) => {
 	if (req.user) {
 		const user = await User.findOne({ email: req.user.email });
 		user.boards.push({ name: req.body.name });
-		user.save();
-		res.status(200).json({ message: "board created" });
+		const updatedBoards = await user.save();
+    res.status(200).json({ message: "board created" });
 	} else {
 		res.status(403).json({ error: "User not authenticated" });
 	}
